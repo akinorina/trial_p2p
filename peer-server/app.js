592f483a81a9de68d5659eff6576c846cfb464f2
Akinori Nakata <akinori.nakata@bright-century.com>
2024-07-26 12:42:25 +0900

    reverse-proxy 実装

--- peer-server/app.js
+++ peer-server/app.js
@@ -2,6 +2,7 @@ const { PeerServer } = require("peer");
 
 const peerServer = PeerServer({
   port: 9500,
-  path: '/myapp',
-  // proxied: true
+  path: '/peer',
+  proxied: true
+  // corsOptions: '*'
 });
